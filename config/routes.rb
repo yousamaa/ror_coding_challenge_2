6829df71e144b3453b7b9af01f5a7230d2c4690e
Usama Yousaf <usamayousaf177@gmail.com>
2024-03-26 13:56:43 -0700

    Modified routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api do
+    resources :tasks, only: [:index, :update, :create, :destroy] do
+      member do
+        post :assign, to: 'tasks#assign'
+        put :progress, to: 'tasks#progress'
+      end
+      collection do
+        get :overdue
+        get :by_status, path: 'status/:status', action: :status
+        get :completed, action: :completed
+        get :statistics
+      end
+    end
+    resources :users, only: [] do
+      member do
+        get :tasks
+      end
+    end
+  end
 end
